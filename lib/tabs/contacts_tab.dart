1b4afc843c94875e5ccb4e33ffc1c1b2b27700ac
PauloV-OFonseca <pfonseca485@gmail.com>
2020-12-31 09:12:34 -0300

    Criando aba de contatos

--- lib/tabs/contacts_tab.dart
+++ lib/tabs/contacts_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/model/user.dart';
 
 class ContactsTab extends StatefulWidget {
   @override
@@ -7,6 +8,17 @@ class ContactsTab extends StatefulWidget {
 
 class _ContactsTabState extends State<ContactsTab> {
 
+  List<User> listaConversas = [
+    User(
+      "Pessoa 1", "A", "123"
+    ),
+    User(
+      "Pessoa 2", "B", "456"
+    ),
+    User(
+      "Pessoa 3", "C", "789"
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +26,35 @@ class _ContactsTabState extends State<ContactsTab> {
       appBar: AppBar(
         title: const Text('Contatos'),
       ),
+      body: ListView.builder(
+        itemCount: listaConversas.length,
+        itemBuilder: (context, index){
+
+          User user = listaConversas[index];
+
+          return ListTile(
+            contentPadding:EdgeInsets.fromLTRB(16, 1, 16, 1),
+            leading: CircleAvatar(
+              maxRadius: 20,
+              backgroundColor: Colors.grey
+            ),
+            title: Text(
+              user.nome,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16
+              ),
+            ),
+            subtitle: Text(
+              user.recado,
+              style: TextStyle(
+                fontSize: 14
+              ),
+            ),
+          );
+        }
+      )
     );
   }
+
 }
