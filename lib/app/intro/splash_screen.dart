6133cf3921bbf2a4faa3ee46afe65443149033e0
PauloV-OFonseca <pfonseca485@gmail.com>
2021-01-07 10:35:33 -0300

    Criação da tela de login

--- lib/app/intro/splash_screen.dart
+++ lib/app/intro/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(
         Duration(seconds: 3),
         () {
-          Navigator.pushReplacementNamed(context, "/home");
+          Navigator.pushReplacementNamed(context, "/login");
         });
   }
 
--- lib/app/shared/utils/route_generator.dart
+++ lib/app/shared/utils/route_generator.dart
@@ -4,6 +4,8 @@ import 'package:whatsapp_clone/app/home/pages/chat/pages/chat_room/chat_room.dar
 import 'package:whatsapp_clone/app/home/pages/chat/pages/chat_room/pages/detail_user.dart';
 import 'package:whatsapp_clone/app/home/pages/status/pages/status_room.dart';
 import 'package:whatsapp_clone/app/home/pages/chat/pages/contacts_tab.dart';
+import 'package:whatsapp_clone/app/intro/pages/login/login.dart';
+import 'package:whatsapp_clone/app/intro/pages/login/register/register.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -22,6 +24,10 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => DetailUser(args));
       case "/statusroom":
         return MaterialPageRoute(builder: (_) => StatusRoom(args));
+      case "/login":
+        return MaterialPageRoute(builder: (_) => Login());
+      case "/register":
+        return MaterialPageRoute(builder: (_) => Register());
       default:
         return MaterialPageRoute(builder: (_) => Home());
     }
