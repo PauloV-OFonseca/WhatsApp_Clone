e6c5e83d6737c21a59dfea4b38c19d5351167481
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2021-01-07 11:23:03 -0300

    Tela de login

--- lib/app/intro/pages/login/login.dart
+++ lib/app/intro/pages/login/login.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'components/login_body.dart';
+
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("ok"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: LoginBody(),
     );
   }
 }
