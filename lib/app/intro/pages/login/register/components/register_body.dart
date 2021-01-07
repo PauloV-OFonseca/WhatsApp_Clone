e93ead915e99868b521f49caad1eff2e3bf9d8c2
PauloV-OFonseca <pfonseca485@gmail.com>
2021-01-07 12:31:27 -0300

    Consertando bottom overflow

--- lib/app/intro/pages/login/register/components/register_body.dart
+++ lib/app/intro/pages/login/register/components/register_body.dart
@@ -9,49 +9,51 @@ class _RegisterBodyState extends State<RegisterBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
-            child: TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Nome",
-                border: OutlineInputBorder(),
-              ),
-            )
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
-            child: TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            )
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
-            child: TextField(
-              obscureText: true,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                border: OutlineInputBorder(),
-              ),
-            )
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
-            child: RaisedButton(
-              child: Text(
-                "Cadastrar",
-              ),
-              onPressed: (){},
-            )
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Nome",
+                  border: OutlineInputBorder(),
+                ),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
+              child: TextField(
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  border: OutlineInputBorder(),
+                ),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
+              child: RaisedButton(
+                child: Text(
+                  "Cadastrar",
+                ),
+                onPressed: (){},
+              )
+            ),
+          ],
+        ),
       )
     );
   }
