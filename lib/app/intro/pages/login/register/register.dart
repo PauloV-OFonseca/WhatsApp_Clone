4e128933776db236b36645fb8d6787827c197947
PauloV-OFonseca <pfonseca485@gmail.com>
2021-01-11 14:24:25 -0300

    Alteração do body em Register

--- lib/app/intro/pages/login/register/register.dart
+++ lib/app/intro/pages/login/register/register.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
-import 'components/register_body.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'components/register_form.dart';
+import 'register_controller.dart';
 
 class Register extends StatelessWidget {
+  final controller = RegisterController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cadastro")
       ),
-      body: RegisterBody(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            RegisterForm(
+              title: "Nome",
+              onChanged: controller.changeName,
+              errorText: controller.validateName,
+              isPassword: false,
+            ),
+            RegisterForm(
+              title: "Email",
+              onChanged: controller.changeEmail,
+              errorText: controller.validateEmail,
+              isPassword: false,
+            ),
+            RegisterForm(
+              title: "Senha",
+              onChanged: controller.changePassword,
+              errorText: controller.validatePassword,
+              isPassword: true,
+            ),
+            Observer(
+              builder: (_) {
+                return RaisedButton(
+                  onPressed: controller.isValid
+                      ? () {
+                          controller.createUser(context);
+                        }
+                      : null,
+                  child: Text("Cadastrar"),
+                );
+              },
+            ),
+          ],
+        ),
+      )
     );
   }
 }
