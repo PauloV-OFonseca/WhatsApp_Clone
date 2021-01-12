1768ae9afefce6924ecd8ee2bb7194d20475dfff
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2021-01-12 14:59:51 -0300

    Colocando o observer no pai e não no filho

--- lib/app/intro/pages/login/components/login_form.dart
+++ lib/app/intro/pages/login/components/login_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 
 class LoginForm extends StatelessWidget {
   final String title;
@@ -12,20 +11,15 @@ class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
-      child: Observer(
-        builder: (_) {
-          return TextFormField(
-            validator: validator,
-            onChanged: onChanged,
-            obscureText: isPassword,
-            decoration: InputDecoration(
-              labelText: title,
-              border: OutlineInputBorder(),
-            ),
-          );
-        },
-      ),
-    );
+        padding: const EdgeInsets.fromLTRB(8, 20, 8, 8),
+        child: TextFormField(
+          validator: validator,
+          onChanged: onChanged,
+          obscureText: isPassword,
+          decoration: InputDecoration(
+            labelText: title,
+            border: OutlineInputBorder(),
+          ),
+        ));
   }
 }
