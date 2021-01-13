6e2b0b16a9f7b8a83617bd7fc64e414ea1869e0d
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2021-01-13 11:40:15 -0300

    trocando o listview builder por map

--- lib/app/home/pages/chat/chats_tab.dart
+++ lib/app/home/pages/chat/chats_tab.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/app/home/pages/chat/chat_tab_controller.dart';
 import 'package:whatsapp_clone/app/shared/components/generic_avatar.dart';
 import 'package:whatsapp_clone/app/shared/consts/texts_styles.dart';
-import 'package:whatsapp_clone/app/shared/utils/mocks.dart';
-import 'package:whatsapp_clone/model/user.dart';
 
 class ChatsTab extends StatefulWidget {
   @override
@@ -10,15 +9,13 @@ class ChatsTab extends StatefulWidget {
 }
 
 class _ChatsTabState extends State<ChatsTab> {
-  final listUser = Mocks().getUsers();
+  final controller = ChatTabController();
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: listUser.length,
-        itemBuilder: (context, index) {
-          User user = listUser[index];
-
+    return Column(
+      children: [
+        ...controller.chatList.map((user) {
           return ListTile(
             onTap: () {
               Navigator.pushNamed(context, "/chatroom", arguments: user);
@@ -34,6 +31,30 @@ class _ChatsTabState extends State<ChatsTab> {
               style: TextsStyles.GENERIC_TEXT_STYLE,
             ),
           );
-        });
+        }),
+      ],
+    );
+
+    // ListView.builder(
+    //     itemCount: controller.chatList.length,
+    //     itemBuilder: (context, index) {
+    //       User user = controller.chatList[index];
+
+    //       return ListTile(
+    //         onTap: () {
+    //           Navigator.pushNamed(context, "/chatroom", arguments: user);
+    //         },
+    //         contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+    //         leading: GenericAvatar().circleProfile(user.foto),
+    //         title: Text(
+    //           user.nome,
+    //           style: TextsStyles.BOLD_TITLE_STYLE,
+    //         ),
+    //         subtitle: Text(
+    //           user.recado,
+    //           style: TextsStyles.GENERIC_TEXT_STYLE,
+    //         ),
+    //       );
+    //     });
   }
 }
