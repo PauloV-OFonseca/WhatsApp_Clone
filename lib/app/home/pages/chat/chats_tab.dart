6f57bb5b13fcd6fd9fa1188584608deb58bbe1d4
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2021-01-13 14:19:39 -0300

    view do chat tab

--- lib/app/home/pages/chat/chats_tab.dart
+++ lib/app/home/pages/chat/chats_tab.dart
@@ -16,45 +16,69 @@ class _ChatsTabState extends State<ChatsTab> {
     return Column(
       children: [
         ...controller.chatList.map((user) {
-          return ListTile(
-            onTap: () {
-              Navigator.pushNamed(context, "/chatroom", arguments: user);
-            },
-            contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-            leading: GenericAvatar().circleProfile(user.foto),
-            title: Text(
-              user.nome,
-              style: TextsStyles.BOLD_TITLE_STYLE,
-            ),
-            subtitle: Text(
-              user.recado,
-              style: TextsStyles.GENERIC_TEXT_STYLE,
-            ),
+          return Column(
+            children: [
+              ListTile(
+                onTap: () {
+                  Navigator.pushNamed(context, "/chatroom", arguments: user);
+                },
+                contentPadding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+                leading: GenericAvatar().circleProfile(user.foto),
+                title: Text(
+                  user.nome,
+                  style: TextsStyles.BOLD_TITLE_STYLE,
+                ),
+                subtitle: Text(
+                  user.recado,
+                  style: TextsStyles.GENERIC_TEXT_STYLE,
+                ),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "08:22",
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.volume_off),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                              color: Colors.green, shape: BoxShape.circle),
+                          child: Center(
+                            child: Text(
+                              "2",
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(85, 0, 10, 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(width: 0.20, color: Colors.grey),
+                    ),
+                  ),
+                ),
+              )
+            ],
           );
         }),
       ],
     );
-
-    // ListView.builder(
-    //     itemCount: controller.chatList.length,
-    //     itemBuilder: (context, index) {
-    //       User user = controller.chatList[index];
-
-    //       return ListTile(
-    //         onTap: () {
-    //           Navigator.pushNamed(context, "/chatroom", arguments: user);
-    //         },
-    //         contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-    //         leading: GenericAvatar().circleProfile(user.foto),
-    //         title: Text(
-    //           user.nome,
-    //           style: TextsStyles.BOLD_TITLE_STYLE,
-    //         ),
-    //         subtitle: Text(
-    //           user.recado,
-    //           style: TextsStyles.GENERIC_TEXT_STYLE,
-    //         ),
-    //       );
-    //     });
   }
 }
