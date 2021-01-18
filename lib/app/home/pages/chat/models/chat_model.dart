749533fcdb9ce840f24da7eeede375143286c9cc
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2021-01-18 11:32:45 -0300

    Integração da lista de conversas do usuario

--- lib/app/home/pages/chat/models/chat_model.dart
+++ lib/app/home/pages/chat/models/chat_model.dart
@@ -9,83 +9,83 @@ ChatModel chatModelFromJson(String str) => ChatModel.fromJson(json.decode(str));
 String chatModelToJson(ChatModel data) => json.encode(data.toJson());
 
 class ChatModel {
+  String remetenteId;
+  String conversaId;
+  String imagem;
+  int mensagensNaoLidas;
+  String nome;
+  bool silenciado;
+  UltimaMensagem ultimaMensagem;
+  int status;
+
   ChatModel({
-    this.foto,
-    this.id,
+    this.remetenteId,
+    this.conversaId,
+    this.imagem,
     this.mensagensNaoLidas,
+    this.nome,
     this.silenciado,
-    this.titulo,
     this.ultimaMensagem,
-    this.visualizado,
+    this.status,
   });
 
-  String foto;
-  String id;
-  int mensagensNaoLidas;
-  bool silenciado;
-  String titulo;
-  UltimaMensagem ultimaMensagem;
-  bool visualizado;
-
   ChatModel copyWith({
-    String foto,
-    String id,
-    int mensagensNaoLdias,
+    String remetenteId,
+    String conversaId,
+    String imagem,
+    int mensagensNaoLidas,
+    String nome,
     bool silenciado,
-    String titulo,
     UltimaMensagem ultimaMensagem,
-    bool visualizado,
+    int status,
   }) =>
       ChatModel(
-        foto: foto ?? this.foto,
-        id: id ?? this.id,
-        mensagensNaoLidas: mensagensNaoLdias ?? this.mensagensNaoLidas,
+        remetenteId: remetenteId ?? this.remetenteId,
+        conversaId: conversaId ?? this.conversaId,
+        imagem: imagem ?? this.imagem,
+        mensagensNaoLidas: mensagensNaoLidas ?? this.mensagensNaoLidas,
+        nome: nome ?? this.nome,
         silenciado: silenciado ?? this.silenciado,
-        titulo: titulo ?? this.titulo,
         ultimaMensagem: ultimaMensagem ?? this.ultimaMensagem,
-        visualizado: visualizado ?? this.visualizado,
+        status: status ?? this.status,
       );
 
-  factory ChatModel.fromJson(Map<String, dynamic> json) => ChatModel(
-        foto: json["foto"] == null ? null : json["foto"],
-        id: json["id"] == null ? null : json["id"],
-        mensagensNaoLidas: json["mensagens_nao_lidas"] == null
-            ? null
-            : json["mensagens_nao_lidas"],
-        silenciado: json["silenciado"] == null ? null : json["silenciado"],
-        titulo: json["titulo"] == null ? null : json["titulo"],
-        ultimaMensagem: json["ultima_mensagem"] == null
-            ? null
-            : UltimaMensagem.fromJson(json["ultima_mensagem"]),
-        visualizado: json["visualizado"] == null ? null : json["visualizado"],
+  factory ChatModel.fromJson(Map<dynamic, dynamic> json) => ChatModel(
+        remetenteId: json["remetente_id"],
+        conversaId: json["conversa_id"],
+        imagem: json["imagem"],
+        mensagensNaoLidas: json["mensagens_nao_lidas"],
+        nome: json["nome"],
+        silenciado: json["silenciado"],
+        ultimaMensagem: UltimaMensagem.fromJson(json["ultima_mensagem"]),
+        status: json["status"],
       );
 
   Map<String, dynamic> toJson() => {
-        "foto": foto == null ? null : foto,
-        "id": id == null ? null : id,
-        "mensagens_nao_lidas":
-            mensagensNaoLidas == null ? null : mensagensNaoLidas,
-        "silenciado": silenciado == null ? null : silenciado,
-        "titulo": titulo == null ? null : titulo,
-        "ultima_mensagem":
-            ultimaMensagem == null ? null : ultimaMensagem.toJson(),
-        "visualizado": visualizado == null ? null : visualizado,
+        "remetente_id": remetenteId,
+        "conversa_id": conversaId,
+        "imagem": imagem,
+        "mensagens_nao_lidas": mensagensNaoLidas,
+        "nome": nome,
+        "silenciado": silenciado,
+        "ultima_mensagem": ultimaMensagem.toJson(),
+        "status": status,
       };
 }
 
 class UltimaMensagem {
+  int horario;
+  String remetente;
+  String texto;
+
   UltimaMensagem({
     this.horario,
     this.remetente,
     this.texto,
   });
 
-  String horario;
-  String remetente;
-  String texto;
-
   UltimaMensagem copyWith({
-    String horario,
+    int horario,
     String remetente,
     String texto,
   }) =>
@@ -95,15 +95,15 @@ class UltimaMensagem {
         texto: texto ?? this.texto,
       );
 
-  factory UltimaMensagem.fromJson(Map<String, dynamic> json) => UltimaMensagem(
-        horario: json["horario"] == null ? null : json["horario"],
-        remetente: json["remetente"] == null ? null : json["remetente"],
-        texto: json["texto"] == null ? null : json["texto"],
+  factory UltimaMensagem.fromJson(Map<dynamic, dynamic> json) => UltimaMensagem(
+        horario: json["horario"],
+        remetente: json["remetente"],
+        texto: json["texto"],
       );
 
   Map<String, dynamic> toJson() => {
-        "horario": horario == null ? null : horario,
-        "remetente": remetente == null ? null : remetente,
-        "texto": texto == null ? null : texto,
+        "horario": horario,
+        "remetente": remetente,
+        "texto": texto,
       };
 }
