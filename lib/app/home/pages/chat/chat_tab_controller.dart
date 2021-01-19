60de6abc39ab8c3507268290525229d3a6894b90
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2021-01-19 15:56:37 -0300

    colocando lista de mensagem no controller

--- lib/app/home/pages/chat/chat_tab_controller.dart
+++ lib/app/home/pages/chat/chat_tab_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:mobx/mobx.dart';
 import 'package:whatsapp_clone/app/home/pages/chat/models/chat_model.dart';
+import 'package:whatsapp_clone/app/home/pages/chat/models/message_model.dart';
 import 'package:whatsapp_clone/app/shared/services/auth_service.dart';
 
 import 'services/chat_service.dart';
@@ -17,8 +18,36 @@ abstract class _ChatTabControllerBase with Store {
   @observable
   ObservableList<ChatModel> chatList = <ChatModel>[].asObservable();
 
+  @observable
+  String userUID;
+
+  @observable
+  ObservableList<MessageModel> messagesList = [
+    MessageModel(
+        texto: "Olá, tudo bem?",
+        horario: 1588700170343,
+        remetente: "KzjHJORIt7OFaaGCVG30jJHYsbP2",
+        status: 2),
+    MessageModel(
+        texto: "Tudo bem e com vc?",
+        horario: 1588700170343,
+        remetente: "zjHJORIt7OFaaGCVG30jJHYsbP2",
+        status: 2),
+    MessageModel(
+        texto: "Flutter é top",
+        horario: 1588700170343,
+        remetente: "zjHJORIt7OFaaGCVG30jJHYsbP2",
+        status: 2),
+    MessageModel(
+        texto: "Verdade!!!! MUITO TOP",
+        horario: 1588700170343,
+        remetente: "KzjHJORIt7OFaaGCVG30jJHYsbP2",
+        status: 2),
+  ].asObservable();
+
   _ChatTabControllerBase() {
     getUid().then((uid) {
+      userUID = uid;
       subscription = chatService
           .getChatListByUID(uid)
           .listen((event) => chatList = event.asObservable());
